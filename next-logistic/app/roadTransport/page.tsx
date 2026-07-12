a7b4c0212d9efe3964b0bd862963fbc8858fbf0e
donni88c <cs_neeko@yahoo.com>
2026-07-12 13:02:39 +0300

    feat: add RoadTransport page and HeroRoad component with content and image

---
+++ next-logistic/app/roadTransport/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroRoad from "@/components/HeroRoad";
+import Footer from "@/components/Footer";
+
+const RoadTransport = () => {
+ return (
+    <div className="h-screen">
+      <HeroRoad />
+        <h1 className="text-white">Road Transport</h1>
+        <Footer />
+    </div>
+ )
+};
+
+export default RoadTransport;
---
+++ next-logistic/components/HeroRoad.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+const HeroRoad = () => {
+  return (
+    <section className="relative h-screen overflow-hidden w-full bg-[#0b3d91] ">
+      <div className="absolute inset-0 bg-linear-to-r from-blue-900/80 to-blue-600/60"></div>
+      <Image
+        src="/road-transport-trucks.png"
+        alt="Hero Image"
+        fill
+        priority
+        className="object-cover opacity-100"
+      />
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4 ">
+        <div className=" bg-blue-500 rounded w-1/2 ">
+          <h2> Leader in land transport</h2>
+          <h4>Uncompromising Logistics</h4>
+
+          <p>
+            The largest logistics group in the Balkans with established
+            transport networks across Europe and the Middle East. We organize
+            road transport with over 7,800 subcontractors and a fleet of Euro 6,
+            LNG, and CNG vehicles. We provide secure, sustainable, and
+            environmentally friendly logistics solutions with full
+            predictability and uncompromising accuracy. Our in-house tracking
+            and real-time management systems ensure maximum transport
+            efficiency.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroRoad;
