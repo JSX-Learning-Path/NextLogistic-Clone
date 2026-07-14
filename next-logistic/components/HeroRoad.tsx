ebcde9f5f1eac19d4f71650da07ddc7dd394db45
donnie88c <cs_neeko@yahoo.com>
2026-07-14 15:40:48 +0300

    style: update HeroRoad component layout and improve text styling for better readability

--- next-logistic/components/HeroRoad.tsx
+++ next-logistic/components/HeroRoad.tsx
@@ -2,21 +2,26 @@ import Image from "next/image";
 
 const HeroRoad = () => {
   return (
-    <section className="relative h-screen overflow-hidden w-full bg-[#0b3d91] ">
-      <div className=" h-screen absolute inset-0 bg-gradient-to-r from-blue-900/80 to-blue-600/60"></div>
+    <section className="relative w-full overflow-hidden bg-[#0b3d91] min-h-[90svh]  lg:min-h-screen">
+      <div className="absolute inset-0 z-10 bg-gradient-to-r from-black/10 to-black/10"></div>
       <Image
         src="/road-transport-trucks.png"
         alt="Hero Image"
         fill
         priority
-        className="object-cover opacity-100"
+        className="object-cover object-center"
       />
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4 ">
-        <div className=" bg-blue-500 rounded w-1/2 ">
-          <h2 className="uppercase"> Leader in land transport</h2>
-          <h4>Uncompromising Logistics</h4>
+      <div className="absolute inset-x-0 bottom-0 z-10 text-center text-white pb-2 mx-3 xl:bottom-auto xl:top-1/2 xl:-translate-y-1/2 xl:w-1/2 xl:mx-auto">
+        <div className="bg-blue-600 rounded-xl w-full opacity-90 px-5 ">
+          <h2 className="uppercase text-3xl font-bold mb-2 md:text-3xl md:mb-3 lg:text-5xl lg:mb-2">
+            {" "}
+            Leader in road transport
+          </h2>
+          <h4 className="text-xl font-semibold uppercase">
+            Uncompromising Logistics
+          </h4>
 
-          <p className="pt-10">
+          <p className="pt-5 text-white text-sm font-semibold md:text-sm ">
             The largest logistics group in the Balkans with established
             transport networks across Europe and the Middle East. We organize
             road transport with over 7,800 subcontractors and a fleet of Euro 6,
